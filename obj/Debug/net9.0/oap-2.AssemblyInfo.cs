9591c1ad396f436f244a79dfba2c2cf1805721b8
Makarov628 <frostmakarov@gmail.com>
2025-12-20 22:58:08 +0500

    4) Open-Closed

--- obj/Debug/net9.0/oap-2.AssemblyInfo.cs
+++ obj/Debug/net9.0/oap-2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("oap-2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d6402852d1fc519d33d6af7cae848498c7718dc5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+050bf96038ffe0623e7f45ceda9b05a41f6247e7")]
 [assembly: System.Reflection.AssemblyProductAttribute("oap-2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("oap-2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
