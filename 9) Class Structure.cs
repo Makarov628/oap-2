a486ca89f1b37b1edb419c484aebda6b0e2a61a8
Dmitriy Rylskiy <frostmakarov@gmail.com>
2026-02-06 22:38:57 +0500

    Add General class with cloning and serialization methods

---
+++ 9) Class Structure.cs
@@ -0,0 +1,63 @@
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text.Json;
+
+namespace OOAP_2._9;
+
+abstract class Any : General {}
+
+abstract class General : object
+{
+    public void ShallowCopyTo<TTarget>(ref TTarget target)
+    {
+        target = (TTarget) MemberwiseClone();
+    }
+
+    public void DeepCopyTo<TTarget>(ref TTarget target)
+    {
+        target = DeepClone<TTarget>();
+    }
+
+    public TResult DeepClone<TResult>()
+    {
+        using var stream = ToMemoryStream();
+        var formatter = new BinaryFormatter();
+
+        return (TResult) formatter.Deserialize(stream);
+    }
+
+    public bool Equals(General obj) => base.Equals(obj);
+
+    public bool DeepEquals(General obj)
+    {
+        using var thisStream = ToMemoryStream();
+        using var objStream = obj.ToMemoryStream();
+
+        if (thisStream.Length != objStream.Length)
+            return false;
+
+        var thisByteArray = thisStream.ToArray();
+        var objByteArray = objStream.ToArray();
+
+        return thisByteArray.SequenceEqual(objByteArray);
+    }
+
+    public string Serialize() => JsonSerializer.Serialize(this);
+
+    public TResult? Deserialize<TResult>(string json) => JsonSerializer.Deserialize<TResult>(json);
+
+    public string Print() => Serialize();
+
+    public bool CheckType(Type type) => GetType() == type;
+    public new Type GetType() => base.GetType();
+
+    private MemoryStream ToMemoryStream()
+    {
+        var formatter = new BinaryFormatter();
+        var stream = new MemoryStream();
+
+        formatter.Serialize(stream, this);
+        stream.Position = 0;
+
+        return stream;
+    }
+}
